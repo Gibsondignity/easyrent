521f2d8bd87f307922b526a36e8ef7a7711e558b
Gibson Dignity <gibsondignty@gmail.com>
2022-07-25 14:36:48 +0000

    fixing styles not working

--- src/components/About/WhyChooseUs/index.js
+++ src/components/About/WhyChooseUs/index.js
@@ -8,36 +8,36 @@ const WhyChooseUs = () => {
         <center>
             <h1 className={style.chooseus}>Why Choose Us</h1>
         </center>
-
+ 
 
         <div className={style.section_4}>
 
             <div className={style.container}>
-                <center><h3>Affordability</h3></center>
+                <h3>Affordability</h3>
                 <hr/>
                 <p>Our price is very competitive and we are best on the market.</p>
             </div>
 
             <div className={style.container}>
-                <center><h3>Experience</h3></center>
+                <h3>Experience</h3>
                 <hr/>
                 <p>We&apos;ve been doing what we do best—we know what we&apos;re doing because we have the industry experience.</p>
             </div>
 
             <div className={style.container}>
-                <center><h3>Quality</h3></center>
+                <h3>Quality</h3>
                 <hr/>
                 <p>From the best skilled professional, process and technology, EasyRent is shaping the rental industry in Ghana. Together, we are changing the status quo.</p>
             </div>
 
             <div className={style.container}>
-                <center><h3>Conveniences</h3></center>
+                <h3>Conveniences</h3>
                 <hr/>
                 <p>At EasyRent, apartment selection, verification of financial resources, contract signing and payment can all be done online with just a click.</p>
             </div>
 
             <div className={style.container}>
-                <center><h3>Trust</h3></center>
+                <h3>Trust</h3>
                 <hr/>
                 <p>Our customers rate us high and are very mush satisfy with our services and product.</p>
             </div>
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 import { Layout } from 'components/Dashboard/Layout/Layout';
 import { Container } from 'react-bootstrap';
+import { useEffect } from 'react';
+
 
 const dashboard = () => {
+
   return (
     <div>
       <Layout >
